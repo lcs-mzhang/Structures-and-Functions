61ce5d2be9e1b1a5ff6ebd381eae3e4022b895b2
Zhang Matt <mzhang@lcs.on.ca>
2017-11-24 12:54:20 -0500

    Solved the challenge!

--- Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
+++ Structures and Functions.playground/Pages/Exercise.xcplaygroundpage/Contents.swift
@@ -21,3 +21,49 @@ import Foundation
 */
 
 // Begin here... once you teach the computer to do this, you will never need to do it by hand again! :)
+// until period 3 when i have to do a math test
+
+struct Point
+{
+    var x : Double = 0.0
+    var y : Double = 0.0
+}
+
+struct Slope
+{
+    var rise : Double = 0.0
+    var run : Double = 0.0
+    var yInt : Double = 0.0
+}
+
+var point1 = Point(x: 6, y: 1.5)
+var slope1 = Slope(rise: -1, run: 2, yInt: 19/2)
+
+func Solve(between lineWithSlope: Slope, Position: Point) -> Double
+{
+    let m = -1*(lineWithSlope.run/lineWithSlope.rise)
+    let b = (-1 * m * Position.x) + Position.y
+    
+    
+
+    let otherM = lineWithSlope.rise/lineWithSlope.run
+    let otherB = lineWithSlope.yInt
+    
+
+    let poiX = (otherM * Position.x + otherB - b)/m
+    let poiY = otherM * poiX + otherB
+    
+    let distance = sqrt(pow(Position.x - poiX, 2)+pow(Position.y - poiY, 2))
+    
+    return distance
+}
+
+
+Solve(between: slope1, Position: point1)
+
+
+
+//find perp slope
+//sub in point
+//find poi
+//calculate line distance
